3311505b415103f7d5f8accfeb5a8d64a08a0df0
Jackelin <jackelinvasquezguzman@gmail.com>
2025-07-16 22:00:17 -0600

    commit inicio menú
    avance

--- Actividad 3.py
+++ Actividad 3.py
@@ -1,1 +1,6 @@
 #SISTEMA ADMINISTRACIÓN ESTUDIANTES.
+
+while True:
+    print("---Menú de Registro de Estudiantes---")
+    print("1.Agregar Estudiante.\n 2.Eliminar Estudiante.\n 3.Mostrar Estudiantes\n 4.Salir")
+    opcion=input("Seleccione una opción:")

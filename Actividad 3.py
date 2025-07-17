e3a55b22abc62f237885fea2bb57171f4c87540b
Jackelin <jackelinvasquezguzman@gmail.com>
2025-07-16 22:16:27 -0600

    commit opcion 1

--- Actividad 3.py
+++ Actividad 3.py
@@ -1,6 +1,17 @@
 #SISTEMA ADMINISTRACIÓN ESTUDIANTES.
+estudiantes=[]
 
 while True:
     print("---Menú de Registro de Estudiantes---")
     print("1.Agregar Estudiante.\n 2.Eliminar Estudiante.\n 3.Mostrar Estudiantes\n 4.Salir")
     opcion=input("Seleccione una opción:")
+
+    if opción=="1":
+        estudiante_agregar= input("Ingrese el nombre de estudiante a agregar:")
+        estudiantes.append(estudiante_agregar)
+        print(f"{estudiante_agregar} agregado exitosamente!")
+
+
+
+
+

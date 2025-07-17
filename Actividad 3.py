6288315765025a69d64698d8c0061773987e43d4
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-16 23:22:08 -0600

    commit op extra

--- Actividad 3.py
+++ Actividad 3.py
@@ -3,11 +3,11 @@ estudiantes=[]
 
 while True:
     print("---Menú de Registro de Estudiantes---")
-    print("1.Agregar Estudiante.\n 2.Eliminar Estudiante.\n 3.Mostrar Estudiantes\n 4.Salir")
+    print("1.Agregar Estudiante.\n 2.Eliminar Estudiante.\n 3.Mostrar Estudiantes\n 4.Calculo de Promedio \n 5.Salir")
     opcion=input("Seleccione una opción:")
 
     if opcion=="1":
-        estudiante_agregar= input("Ingrese el nombre de estudiante a agregar:")
+        estudiante_agregar= input("Ingrese el nombre de estudiante a agregar:").capitalize()
         estudiantes.append(estudiante_agregar)
         print(f"{estudiante_agregar} agregado exitosamente!")
 
@@ -22,8 +22,16 @@ while True:
             print(f"estudiante no.{student+1}__{estudiantes[student]}")
 
     if opcion =="4":
+        a= input("ingrese primera nota: ")
+        b=input("ingrese segunda nota:")
+        promedio= a+b /2
+        print (promedio)
+
+
+    if opcion =="5":
         print(f"___Saliendo del sistema.........")
-        break
+    break
+
 
 
 

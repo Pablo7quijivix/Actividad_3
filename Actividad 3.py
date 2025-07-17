71bf7ec1e5cce2b88d99c3ab94bd3a6f352304b5
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-16 22:32:34 -0600

    commit ultimas opciones

--- Actividad 3.py
+++ Actividad 3.py
@@ -6,17 +6,23 @@ while True:
     print("1.Agregar Estudiante.\n 2.Eliminar Estudiante.\n 3.Mostrar Estudiantes\n 4.Salir")
     opcion=input("Seleccione una opción:")
 
-    if opción=="1":
+    if opcion=="1":
         estudiante_agregar= input("Ingrese el nombre de estudiante a agregar:")
         estudiantes.append(estudiante_agregar)
         print(f"{estudiante_agregar} agregado exitosamente!")
 
-    if opción =="2":
+    if opcion =="2":
         estudiante_eliminar=input("Ingrese nombre de estudiante a eliminar:")
         if estudiante_eliminar in estudiantes:
             estudiantes.remove(estudiante_eliminar)
         print(f"{estudiante_eliminar} eliminado Exitosamente!")
 
+    if opcion =="3":
+        for listado in estudiantes:
+            print(f"___Mostrando Listado de estudiantes___\n {estudiantes}")
+
+    if opcion =="4":
+        print(f"___Saliendo del sistema.........")
 
 
 
